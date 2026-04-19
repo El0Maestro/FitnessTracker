4eddbadb2e15d3d591dc85858d195ebe6a69b077
Jakub Figaj <jakub.figaj@gmail.com>
2026-04-19 15:29:11 +0200

    zad1 event

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,46 @@
 package pl.wsb.fitnesstracker.event;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.training.internal.ActivityType;
+import pl.wsb.fitnesstracker.user.api.User;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "event")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+
 // TODO: Define the Event entity with appropriate fields and annotations
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "start_time", nullable = false)
+    private String start_time;
+
+    @Column(name = "end_time", nullable = false)
+    private String end_time;
+
+    @Column(name = "country", nullable = false)
+    private String country;
+
+    @Column(name = "city", nullable = false)
+    private String city;
 }
