cd9878d171a1bfab255d7aaba27a180b54ba2ff1
Jakub Figaj <jakub.figaj@gmail.com>
2026-04-19 15:51:52 +0200

    zad4

--- src/main/java/pl/wsb/fitnesstracker/training/internal/TrainingRepository.java
+++ src/main/java/pl/wsb/fitnesstracker/training/internal/TrainingRepository.java
@@ -1,7 +1,15 @@
 package pl.wsb.fitnesstracker.training.internal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.wsb.fitnesstracker.training.api.Training;
 
 public interface TrainingRepository extends JpaRepository<Training, Long> {
+    @Query(
+            value = "SELECT COUNT(*) FROM user_event WHERE event_id = :eventId",
+            nativeQuery = true
+    )
+    long countParticipants(@Param("eventId") Long eventId);
+
 }
