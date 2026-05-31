9bb1506ac4ba2528ed81fdffcf400f626555237c
maestro <kuchalskimariusz@gmail.com>
2026-05-31 14:40:23 +0200

    Dodano wymagany plik dla lab04 v2

---
+++ src/main/java/pl/wsb/fitnesstracker/event/UserEvent.java
@@ -0,0 +1,32 @@
+package pl.wsb.fitnesstracker.event;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pl.wsb.fitnesstracker.user.api.User;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_event")
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    private LocalDateTime registrationDate;
+
+    private String status;
+}
